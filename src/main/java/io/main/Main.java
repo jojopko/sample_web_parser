7879ad4ab6f5537da2e112199e0c605d2084d3f9
jojopko <konstantin.serez@gmail.com>
2022-11-30 19:53:37 +0300

    make parser for example.com

--- src/main/java/io/main/Main.java
+++ src/main/java/io/main/Main.java
@@ -1,23 +1,41 @@
 package io.main;
 
 
+import org.jetbrains.annotations.NotNull;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 
-import java.io.FileOutputStream;
 
 public class Main {
     public static void main(String[] args) {
         Document dom;
         try {
-            FileOutputStream file_output = new FileOutputStream("/tmp/a.html");
             dom = Jsoup.connect("http://example.com/").get();
-            System.out.printf(dom.html());
-//            file_output.write(dom.html().getBytes(StandardCharsets.UTF_8));
-            file_output.close();
+            System.out.println(getHeaderInPlainTextFormat(dom));
+            System.out.println(getContentOfArticleInPlainTextFormat(dom));
         }
         catch (Exception e) {
             e.printStackTrace();
+        }
+    }
+
+    @NotNull
+    private static String getHeaderInPlainTextFormat(Document html_dom){
+        try {
+            return html_dom.getElementsByTag("h1").first().text();
+        }
+        catch (NullPointerException e){
+            return "(null)";
+        }
+    }
+
+    @NotNull
+    private static String getContentOfArticleInPlainTextFormat(Document html_dom){
+        try {
+            return html_dom.getElementsByTag("p").first().text();
+        }
+        catch (NullPointerException e){
+            return "(null)";
         }
     }
 }
